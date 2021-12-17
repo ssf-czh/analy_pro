ace51ecd97cdbc9aa32fdfedd497575acaade62b
ZZZ11223 <95113008+ZZZ11223@users.noreply.github.com>
2021-12-17 23:42:35 +0800

    Add files via upload

---
+++ gui/MyMainWindows.py
@@ -0,0 +1,23 @@
+import sys
+
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from main_1 import Ui_MainWindow
+from model.db import load,init_params
+
+load(r"C:\Users\ZZZ\Documents\GitHub\analy_pro\model\template.xlsx")
+
+class MyMainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyMainWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
+
+if __name__ == '__main__':
+
+
+    app = QApplication(sys.argv)  # 在 QApplication 方法中使用，创建应用程序对象
+    myWin = MyMainWindow()  # 实例化 MyMainWindow 类，创建主窗口
+    myWin.show()
+    sys.exit(app.exec_())  # 结束进程，退出程序
